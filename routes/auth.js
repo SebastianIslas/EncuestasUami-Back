3f4c973dcbdc8935d30e3054b7429b4d1899db75
Jorge Luis <77176041+dxdiag41@users.noreply.github.com>
2023-03-09 20:12:45 -0600

    .

--- routes/auth.js
+++ routes/auth.js
@@ -80,6 +80,8 @@ router.get('/administrador/encuesta/desactivada/:periodo', EncuestasCtrl.consult
 var EncuestasResCtrl = require('../controllers/encuestasResCtrl');
 router.post('/alumno/:matricula/:id_licenciatura/encuestaResuelta', EncuestasResCtrl.agregarEncuestaResVacia); // Servicio temporal
 router.post('/alumno/:matricula/:id_licenciatura/encuestaResuelta', EncuestasResCtrl.recibirEncuestaResuelta);
+router.get('/alumno/ultimaEncuestaRes/:periodo/:matricula', EncuestasResCtrl.consultarUltimaEncuestaRes); 
+router.get('/alumno/encuestasRes/:matricula', EncuestasResCtrl.consultarEncuestasRes); 
 
 var ProfesorCtrl = require('../controllers/profesorCtrl');
 router.post('/admin/profesor/crearProfesor', ProfesorCtrl.crearProfesor);
